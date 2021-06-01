e63c4b02af02623fd595743b7b806b5a1a158a9f
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-06-01 16:29:23 -0500

    app.js initial setup

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: {},
+    followers: []
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/RasmussenBen')
+      .then((res) => {
+        this.setState({...this.state, users: res.data})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+    axios.get('https://api.github.com/users/RasmussenBen/followers')
+      .then((res) => {
+        this.setState({...this.state, followers: res.data})
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>My Github Profile</h1>
+      </div>
+      )
+  }
 }
 
-export default App;
+
+
+export default App
